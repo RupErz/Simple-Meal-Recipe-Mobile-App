6e952b52ad5b85645f906ec98e2dbef0e97043d2
Nghia <nghiavu144@gmail.com>
2025-01-29 14:54:28 -0600

    Adding a screen using Stack Navigator

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import CategoriesScreen from './screens/CategoriesScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 export default function App() {
+  
+  // Now Stack is an object hold 2 property as a COMPONENTS
+  const Stack = createNativeStackNavigator()
+
   return (
     <>
-      <StatusBar style='light'/>
-      <CategoriesScreen />
+      <StatusBar style='dark'/>
+      <NavigationContainer>
+        <Stack.Navigator>
+
+          {/* Allow us to register a screen within a Navigator */}
+          <Stack.Screen name="MealCategories" component={CategoriesScreen} /> 
+            
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
     
   );

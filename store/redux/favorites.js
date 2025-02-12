d5cfc1ca1de8d6f2634c485edc8d58678283c98e
Nghia <nghiavu144@gmail.com>
2025-02-12 17:08:32 -0600

    React redux step 2

---
+++ store/redux/favorites.js
@@ -0,0 +1,22 @@
+// A file to manage logical operations for our Redux storage
+import { createSlice } from '@reduxjs/toolkit';
+
+const favortesSlice = createSlice({
+    name: 'favorites',
+    initialState: {
+        ids: [],
+    },
+    reducers: { // Function to change our state
+        // Every method auto get latest state as a first parameter
+        addFavorite: (state, action) => {
+            state.ids.push(action.payload.id);
+        },
+        removeFavorite: (state) => {
+            state.ids.splice(state.ids.indexOf(action.payload.id), 1);
+        }
+    }
+})
+
+export const addFavorite = favortesSlice.actions.addFavorite;
+export const removeFavorite = favortesSlice.actions.removeFavorite;
+export default favortesSlice.reducer;
--- store/redux/store.js
+++ store/redux/store.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import { configureStore } from '@reduxjs/toolkit'
+import favoriteReducer from './favorites'
 
 export const store = configureStore({
     reducer: {
-
+        favoriteMeals: favoriteReducer
     }
 });
